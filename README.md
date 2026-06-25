2e75ccb10852a4f10dd317b58832a63ffaf550f6
infoanilsahu <info.anilsahu@gmail.com>
2026-06-25 11:00:24 +0530

    add redis to handle multiple websocket server

--- README.md
+++ README.md
@@ -1,15 +1,5 @@
 # pub-sub-chat
 
-To install dependencies:
+scalable websocket server
 
-```bash
-bun install
-```
-
-To run:
-
-```bash
-bun run index.ts
-```
-
-This project was created using `bun init` in bun v1.3.13. [Bun](https://bun.com) is a fast all-in-one JavaScript runtime.
+[!Redis](redis.png)
--- server.ts
+++ server.ts
@@ -1,9 +1,11 @@
 import http, {IncomingMessage, ServerResponse} from "http"
 import WebSocket, { WebSocketServer } from "ws";
+import { redisPublish, redisSubscribe } from "./redis";
 import fs from "fs/promises";
 import path from "path";
 
 const port = process.env.PORT ?? 9000;
+const REDIS_CHANNEL = "ws-message"
 
 const server = http.createServer( async (req: IncomingMessage, res: ServerResponse) => {
     const htmlfile = await fs.readFile(path.resolve('./index.html'), "utf-8")
@@ -13,17 +15,25 @@ const server = http.createServer( async (req: IncomingMessage, res: ServerRespon
 
 const wsServer = new WebSocketServer({server})
 
+redisSubscribe.subscribe(REDIS_CHANNEL)
+
+redisSubscribe.on("message", (channel: string, message: string) => {
+    if( channel === REDIS_CHANNEL ) {
+        wsServer.clients.forEach((client) => {
+            if( client.readyState === WebSocket.OPEN ) {
+                client.send(message.toString())
+            }
+        })
+    }
+})
+
 wsServer.on("connection", async (websocket: WebSocket) => {
     console.log("ws running");
 
-    websocket.on("message", function (data) {
+    websocket.on("message", async function (data) {
         console.log(data.toString());
         
-        wsServer.clients.forEach((client) => {
-            if( client.readyState === websocket.OPEN && client !== websocket ) {
-                client.send(data.toString())
-            }
-        })
+        await redisPublish.publish(REDIS_CHANNEL, data.toString());
     })
 
     websocket.on("close", () => {
